06d126cc3c37e6e77f3c671c7dc351847cd3cbba
dailyrang <69h9@naver.com>
2023-04-10 17:32:17 +0900

    id 중복 확인 후 가입 버튼 활성화

--- src/WinMemberJoin.java
+++ src/WinMemberJoin.java
@@ -15,13 +15,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
 
 public class WinMemberJoin extends JDialog {
 	private JTextField tfid;
-	private JTextField tfpw;
 	private JTextField tfname;
 	private JTextField tfemail;
 	private JTextField tfmobile;
+	private JPasswordField tfPw;
 
 	/**
 	 * Launch the application.
@@ -58,16 +59,12 @@ public class WinMemberJoin extends JDialog {
 		lblPw.setBounds(38, 84, 57, 15);
 		getContentPane().add(lblPw);
 		
-		tfpw = new JTextField();
-		tfpw.setColumns(10);
-		tfpw.setBounds(131, 81, 116, 21);
-		getContentPane().add(tfpw);
-		
 		JLabel lblName = new JLabel("name : ");
 		lblName.setBounds(38, 124, 57, 15);
 		getContentPane().add(lblName);
 		
 		tfname = new JTextField();
+		tfname.setEnabled(false);
 		tfname.setColumns(10);
 		tfname.setBounds(131, 121, 116, 21);
 		getContentPane().add(tfname);
@@ -77,6 +74,7 @@ public class WinMemberJoin extends JDialog {
 		getContentPane().add(lblemail);
 		
 		tfemail = new JTextField();
+		tfemail.setEnabled(false);
 		tfemail.setColumns(10);
 		tfemail.setBounds(131, 164, 266, 21);
 		getContentPane().add(tfemail);
@@ -86,11 +84,13 @@ public class WinMemberJoin extends JDialog {
 		getContentPane().add(lblmobile);
 		
 		tfmobile = new JTextField();
+		tfmobile.setEnabled(false);
 		tfmobile.setColumns(10);
 		tfmobile.setBounds(131, 207, 116, 21);
 		getContentPane().add(tfmobile);
 		
 		JButton btnJoin = new JButton("가입");
+		btnJoin.setEnabled(false);
 		btnJoin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -99,23 +99,72 @@ public class WinMemberJoin extends JDialog {
 //					Connection con = DriverManager.getConnection("jdbc:mysql://192.168.0.102:3306/sqldb","student","1234");
 					System.out.println("DB 연결 완료");
 					Statement stmt = con.createStatement();
+					
+					String temp = tfmobile.getText(); // 01011112222 => 010-1111-2222
+					if(temp.length() ==11)
+						temp = temp.substring(0, 3)+"="+temp.substring(3,7)+"-"+temp.substring(7);
+					else
+						temp = "000-0000-0000";
+					
+//					temp = temp.substring(0, 3)+"="+temp.substring(3,7)+"-"+temp.substring(7); 
+//					System.out.println(temp);
+					
 					String sql = "insert into membertbl values('" + tfid.getText()+"','";
-					sql = sql + tfpw.getText() + "','" + tfname.getText() + "','";
-					sql = sql + tfemail.getText() + "','" + tfmobile.getText() + "')";
+					sql = sql + tfPw.getPassword().toString() + "','" + tfname.getText() + "','";
+					sql = sql + tfemail.getText() + "','" + temp + "')";
 					
 					if(stmt.executeUpdate(sql) <= 0)
 						System.out.println("삽입 오류 발생");
 					
 // 					System.out.println(sql);
 
-				} catch (ClassNotFoundException | SQLException e1) {
-
-					System.out.println("DB 문제");
+				}catch (ClassNotFoundException | SQLException e1) {
+					
+					System.out.println("DB 문제1");
 				}
 			}
 		});
-		btnJoin.setBounds(300, 206, 97, 23);
+		btnJoin.setBounds(300, 195, 97, 34);
 		getContentPane().add(btnJoin);
-	}
+		
+		tfPw = new JPasswordField();
+		tfPw.setEnabled(false);
+		tfPw.setBounds(131, 81, 116, 21);
+		getContentPane().add(tfPw);
+		
+		JButton btnDup = new JButton("중복확인");
+		btnDup.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sqldb","root","1234");
+					System.out.println("DB 연결 완료");
+					Statement stmt = con.createStatement();
+					String sql = "select * from membertbl where id ='" + tfid.getText() +  "'";
+					ResultSet rs = stmt.executeQuery(sql);
+					if(!rs.next()) {
+						enableEdit();
+					}else
+						tfid.setSelectionStart(0); //블록 시작
+						tfid.setSelectionEnd(tfid.getText().length()); //블록 끝
+						tfid.requestFocus();
 
+				} catch (ClassNotFoundException | SQLException e1) {
+
+					System.out.println("DB 문제1");
+				}
+			}
+
+			private void enableEdit() {
+			btnJoin.setEnabled(true);
+			tfPw.setEnabled(true);
+			tfname.setEnabled(true);
+			tfemail.setEnabled(true);
+			tfmobile.setEnabled(true);
+			}
+		});
+
+		btnDup.setBounds(271, 40, 97, 23);
+		getContentPane().add(btnDup);
+	}
 }
--- src/Winlogin.java
+++ src/Winlogin.java
@@ -18,11 +18,11 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+import javax.swing.JPasswordField;
 
 public class Winlogin extends JDialog {
 	private JTextField tfID;
-	private JTextField tfPassword;
+	private JPasswordField tfPassword;
 
 	/**
 	 * Launch the application.
@@ -46,14 +46,14 @@ public class Winlogin extends JDialog {
 	 */
 	public Winlogin() {
 		setTitle("로그인 창");
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 210);
 		
 		JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
-		JLabel lblID = new JLabel("ID : ");
-		lblID.setBounds(31, 34, 97, 15);
+		JLabel lblID = new JLabel("ID:");
+		lblID.setBounds(33, 40, 70, 15);
 		panel.add(lblID);
 		
 		tfID = new JTextField();
@@ -65,74 +65,73 @@ public class Winlogin extends JDialog {
 				}
 			}
 		});
-		tfID.setBounds(158, 28, 116, 21);
+		tfID.setBounds(134, 37, 116, 21);
 		panel.add(tfID);
 		tfID.setColumns(10);
 		
-		JLabel lblPassword = new JLabel("Password :");
-		lblPassword.setBounds(31, 78, 97, 15);
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(33, 97, 70, 15);
 		panel.add(lblPassword);
-		
-		tfPassword = new JTextField();
-		tfPassword.setColumns(10);
-		tfPassword.setBounds(158, 72, 116, 21);
-		panel.add(tfPassword);
 		
 		JButton btnLogin = new JButton("로그인...");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// DB연결
+				// 데이터베이스 연결
 				connectDB();
 			}
 		});
-		btnLogin.setBounds(307, 28, 97, 65);
+		btnLogin.setBounds(271, 36, 97, 76);
 		panel.add(btnLogin);
 		
-		JLabel lblJoinMember = new JLabel("회원가입...");
+		JLabel lblJoinMember = new JLabel(" 회원가입...");
 		lblJoinMember.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				WinMemberJoin winMemberJoin = new WinMemberJoin();
 				winMemberJoin.setModal(true);
-				winMemberJoin.setVisible(true); 
+				winMemberJoin.setVisible(true);
 			}
 		});
-		lblJoinMember.setBounds(158, 119, 116, 15);
+		lblJoinMember.setBounds(134, 146, 116, 15);
 		panel.add(lblJoinMember);
+		
+		tfPassword = new JPasswordField();
+		tfPassword.setBounds(134, 94, 116, 21);
+		panel.add(tfPassword);
 
 	}
 
 	protected void connectDB() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sqldb","root","1234");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sqlDB","root","1234");
 			System.out.println("DB 연결 완료");
 			
-			String sql = "select * from memberTBL where id='" + tfID.getText()+"'";
+			String sql = "select * from memberTBL where id='" + tfID.getText() + "'";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(sql);
 			
 			if(rs.next()) {
 				String pw = rs.getString("password");
-				String sName = rs.getNString("name"); //추가
+				System.out.println(pw + " vs. " + new String(tfPassword.getPassword()));
+				String sName = rs.getString("name");  // 추가
 				
-				if (pw.equals(tfPassword.getText())) {
+				if(pw.equals(new String(tfPassword.getPassword()))) {
 //					dispose();
 					WinMain winMain = new WinMain(sName);
 					winMain.setModal(true);
-					winMain.setVisible(true);
+					winMain.setVisible(true);	
 //					JOptionPane.showMessageDialog(null, "로그인성공");
 				}else {
 					JOptionPane.showMessageDialog(null, "패스워드를 확인하세요");
 					tfPassword.setText("");
 					tfPassword.requestFocus();
-				}
+				}					
 			}
-			else			
+			else
 				JOptionPane.showMessageDialog(null, "아이디를 확인하세요");
-		
+			
 		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
 			System.out.println("DB 문제");
 		}
 		
